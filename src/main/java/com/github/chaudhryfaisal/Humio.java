67d2af4f92a6283ae33d99f97daebd1eabd6de1c
Chaudhry, Faisal <Faisal.Chaudhry@mastercard.com>
2020-02-01 04:19:14 -0600

    Fixed bug if not properly initialized added debug logs if enabled

--- src/main/java/com/github/chaudhryfaisal/Humio.java
+++ src/main/java/com/github/chaudhryfaisal/Humio.java
@@ -9,6 +9,7 @@ import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -22,13 +23,18 @@ import java.util.Map;
 public class Humio {
     public static final String DEFAULT_HOST_NAME = "https://cloud.humio.com";
     public static final String DEFAULT_PATH = "/api/v1/ingest/humio-structured";
+    public static final int DEFAULT_HTTP_SUCCESS_CODE = 200;
 
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     @Builder.Default
+    private boolean debug = false;
+    @Builder.Default
     private String endpoint = DEFAULT_HOST_NAME;
     @Builder.Default
     private String uri = DEFAULT_PATH;
+    @Builder.Default
+    private int successCode = DEFAULT_HTTP_SUCCESS_CODE;
     private String token;
     @Singular
     private Map<String, Object> tags;
@@ -36,16 +42,33 @@ public class Humio {
     private OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new GzipRequestInterceptor()).build();
 
     public void write(final EventPayload eventPayload) {
-        String json = JsonStream.serialize(Collections.singleton(eventPayload));
-        RequestBody body = RequestBody.create(json, JSON);
+        if (debug) {
+            System.out.println("Humio:write eventPayload=" + eventPayload);
+        }
+        if (endpoint == null || endpoint.isEmpty()) {
+            endpoint = DEFAULT_HOST_NAME;
+        }
+        if (uri == null || uri.isEmpty()) {
+            uri = DEFAULT_PATH;
+        }
         Request request = new Request.Builder()
                 .url(endpoint + uri)
-                .post(body)
+                .post(RequestBody.create(JsonStream.serialize(Collections.singleton(eventPayload)), JSON))
                 .addHeader("Authorization", "Bearer " + token)
                 .build();
         try {
-            client.newCall(request).execute();
+            Response resp = client.newCall(request).execute();
+            if (resp.body() != null) {
+                resp.body().close();
+            }
+            if (resp.code() != successCode) {
+                System.err.printf("Humio:WARN statusCode=%d failedEvents=%d\n", resp.code(), eventPayload.getEvents().size());
+            }
+            if (debug) {
+                System.out.println("Humio:write resp=" + resp);
+            }
         } catch (IOException e) {
+            System.out.println("Humio:write " + e.getLocalizedMessage());
             e.printStackTrace();
         }
     }
